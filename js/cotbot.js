c87eaa8c511e79fe662b89fa6c3ea1d9016d25f5
Utah Kate Newman <utah.kate.newman@gmail.com>
2015-04-01 02:26:19 -0700

    button styling, consolidate animations

--- js/cotbot.js
+++ js/cotbot.js
@@ -1,31 +1,13 @@
 $(document).ready(function() {
 
-  // When eyes button is clicked, toggle laser class on brain
-  $(".flash").click(function() {
-    $(".brain").toggleClass('laser');
-  });
 
-  // When color button is clicked...
-  $(".color").click(function() {
-    // Assign each named color a random number 0-255
-    var red = Math.floor(Math.random() * 255);
-    var green = Math.floor(Math.random() * 255);
-    var blue = Math.floor(Math.random() * 255);
-    
-    // Generate RGBA value for css
-    var randomRGBA = 'rgba('+red+','+green+','+blue+',1)';
-    
-    // Change background to random RGBA color
-    $("body").css("background", randomRGBA);
-  });
+  // Define audio variable for use in Hammer Time animation, declare interval and timeout variables for timing in Hammer Time animation
+  var audio = document.createElement('audio');
+    audio.setAttribute('src', 'assets/MCHammer.mp3');
+  var intervalID;
+  var roboWait;
 
-    // Define audio variable for use in Hammer Time animation, declare interval and tiomeout variables for timing in Hammer Time animation
-    var audio = document.createElement('audio');
-      audio.setAttribute('src', 'assets/MCHammer.mp3');
-    var intervalID;
-    var roboWait;
-
-  // When hammer time button clicked, robot dances like MC Hammer, plays or pauses 'U Can't touch this'   
+  // Triggers/pauses hammer dance, music, flashing eyes, and background color change    
   $(".hammer").click(function() {
     if (!roboWait) {
       roboWait = setTimeout( function() {
